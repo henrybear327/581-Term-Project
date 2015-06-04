e5a324a74334eb75e1ff2ade811b84972600ee4b
Henry Tseng <henrybear327@gmail.com>
2015-06-04 05:46:04 +0000

    Create department.txt generator

---
+++ Main project/generator/generate_department.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+/*clang-format-3.5 -i -style=LLVM  generate_student.c && astyle --style=linux
+ * generate_student.c && clang -Wall -Wextra generate_student.c
+ * -ogenerate_student.o*/
+
+int main()
+{
+    /*
+    struct student {
+        long long int student;
+        char name[NAME_SIZE];
+
+        Grade grade;
+        Department choice[6]; // 志願序
+        int current_result;   //目前跑到第幾志願
+
+        Student *next;
+    };
+    */
+    srand(time(NULL));
+
+    FILE *pFile;
+    pFile = fopen("department.txt", "w");
+
+    if (pFile == NULL) {
+        printf("Error opening file. Terminating...\n");
+        return 0;
+    }
+
+    printf("How many department is available? ");
+    int department;
+    scanf("%d", &department);
+    printf("Department ID starting with? ");
+    long long int department_ID;
+    scanf("%lld", &department_ID);
+    printf("Maximum quota per department? ");
+    int quota;
+    scanf("%d", &quota);
+
+    for (int i = 0; i < department; i++) {
+        fprintf(pFile, "%15lld omit %2d\n", department_ID++, rand() % quota);
+    }
+
+    fclose(pFile);
+
+    return 0;
+}
