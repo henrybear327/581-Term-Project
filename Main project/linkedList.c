348c7066879c1f12fef6ca2ddd080146cd383cba
Henry Tseng <henrybear327@gmail.com>
2015-06-05 14:12:12 +0800

    Add rewind() to getInput.c and implement list all data function

--- Main project/linkedList.c
+++ Main project/linkedList.c
@@ -51,3 +51,62 @@ void show_all_node(Department *head)
     }
     printf("---That's all in the linked list---\n");
 }
+
+void search_node(char dataType, void *input_head, long long int ID)
+{
+    if (dataType == 's' || dataType == 'S') { // student
+        Student *head = ((Student *)input_head);
+
+        printf("Searching for student ID %lld\n", ID);
+
+        int flag = 0;
+        while (head) {
+            if (head->ID == ID) {
+                printf("ID %lld found.\n", ID);
+                flag = 1;
+                break;
+            }
+            head = head->next;
+        }
+
+        if (flag) {
+            printf("The info of student ID %lld\n", ID);
+            printf("ID: %15lld, name: %s.\n"
+                   "Grade: Chinese %2d, math %2d, English %2d, social studies %2d, "
+                   "science %2d.\n"
+                   "Choice 1.%5lld 2.%5lld 3.%5lld 4.%5lld 5.%5lld 6.%5lld\n",
+                   head->ID, head->name, head->grade.chinese, head->grade.english,
+                   head->grade.math, head->grade.social_science, head->grade.science,
+                   head->choice[0].department_ID, head->choice[1].department_ID,
+                   head->choice[2].department_ID, head->choice[3].department_ID,
+                   head->choice[4].department_ID, head->choice[5].department_ID);
+        } else {
+            printf("The student ID %lld not found\n", ID);
+        }
+    } else if (dataType == 'd' || dataType == 'D') { // department
+        Department *head = (Department *)input_head;
+
+        printf("Searching for department ID %lld\n", ID);
+
+        int flag = 0;
+        while (head) {
+            if (head->ID == ID) {
+                printf("ID %lld found.\n", ID);
+                flag = 1;
+                break;
+            }
+            head = head->next;
+        }
+
+        if (flag) {
+            printf("The info of department ID %lld\n", ID);
+            printf("ID: %lld, name: %s, quota: %d\n", head->ID, head->name,
+                   head->quota);
+        } else {
+            printf("The department ID %lld not found\n", ID);
+        }
+    } else {
+        printf("This shouldn't happen! %s %d\n", __FILE__, __LINE__);
+        exit(0);
+    }
+}
